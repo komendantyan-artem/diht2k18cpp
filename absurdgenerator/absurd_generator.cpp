7a4672891e214bf47ddeb0d0d5e9074f82fe8927
Artem Komendantian <komendantyan.artem@gmail.com>
2018-11-29 01:33:46 +0300

    Add string_view

--- absurdgenerator/absurd_generator.cpp
+++ absurdgenerator/absurd_generator.cpp
@@ -4,6 +4,7 @@
 #include <iterator>
 #include <vector>
 #include <string>
+#include <string_view>
 #include <map>
 #include <algorithm>
 #include <random>
@@ -12,7 +13,7 @@
 #include "absurd_generator.h"
 
 using std::vector;
-using std::string;
+using std::string_view;
 
 const unsigned AbsurdGenerator::STROPHE_SIZE = 14;
 const unsigned AbsurdGenerator::STROPHE_DEPENDENCY[] = {
@@ -22,14 +23,14 @@ const unsigned AbsurdGenerator::STROPHE_DEPENDENCY[] = {
     13, 12         // gg
 };
 
-bool AbsurdGenerator::reversed_last_word_comparator(const string& first, const string& second) {
+bool AbsurdGenerator::reversed_last_word_comparator(const string_view& first, const string_view& second) {
     assert(!first.empty() && !second.empty());
     const char* p1 = &first[first.size() - 1];
     const char* p2 = &second[second.size() - 1];
-    while (ispunct(*p1)) {
+    while (isspace(*p1) || ispunct(*p1)) {
         --p1;
     }
-    while (ispunct(*p2)) {
+    while (isspace(*p2) || ispunct(*p2)) {
         --p2;
     }
     for (;; --p1, --p2) {
@@ -45,7 +46,7 @@ bool AbsurdGenerator::reversed_last_word_comparator(const string& first, const s
     }
 }
 
-AbsurdGenerator::AbsurdGenerator(const string& filename):
+AbsurdGenerator::AbsurdGenerator(const std::string& filename):
     random_generator(std::chrono::steady_clock::now().time_since_epoch().count()),
     lines_by_strophe(STROPHE_SIZE)
 {
@@ -55,51 +56,58 @@ AbsurdGenerator::AbsurdGenerator(const string& filename):
     }
 }
 
-void AbsurdGenerator::read(const string& filename) {
+void AbsurdGenerator::read(const std::string& filename) {
     std::ifstream fin(filename);
     if (!fin.is_open()) {
         std::cerr << "There is no input file" << std::endl;
         exit(1);
     }
-    vector<string> lines;
-    string line;
-    while (getline(fin, line)) {
-        if (line.empty()) {
-            lines.clear();
-        } else {
-            lines.push_back(line);
-            if (lines.size() == STROPHE_SIZE) {
-                add_strophe(lines);
+    fin.seekg(0, std::ios::end);
+    buffer.reserve(fin.tellg());
+    fin.seekg(0, std::ios::beg);
+    buffer.assign(std::istreambuf_iterator<char>(fin), std::istreambuf_iterator<char>());
+    
+    vector<string_view> lines;
+    for (unsigned start = 0, it = 0; it < buffer.size(); ++it) {
+        if (buffer[it] == '\n') {
+            if (start == it) {
                 lines.clear();
+            } else {
+                lines.emplace_back(&buffer[start], it - start);
+                if (lines.size() == STROPHE_SIZE) {
+                    add_strophe(lines);
+                    lines.clear();
+                }
             }
+            start = it + 1;
         }
     }
 }
 
-void AbsurdGenerator::add_strophe(const vector<string>& lines) {
+void AbsurdGenerator::add_strophe(const vector<string_view>& lines) {
     for (size_t i = 0; i < lines.size(); ++i) {
         lines_by_strophe[i].push_back(lines[i]);
     }
 }
 
-vector<string> AbsurdGenerator::generate_strophe() {
-    vector<string> result;
+vector<string_view> AbsurdGenerator::generate_strophe() {
+    vector<string_view> result;
     for (size_t i = 0; i < STROPHE_SIZE; ++i) {
         if (STROPHE_DEPENDENCY[i] >= i) {
             result.push_back(get_random_line(lines_by_strophe[i]));
         } else {
-            const string& previous = result[STROPHE_DEPENDENCY[i]];
+            const string_view& previous = result[STROPHE_DEPENDENCY[i]];
             result.push_back(get_matched_line(lines_by_strophe[i], previous));
         }
     }
     return result;
 }
 
-string AbsurdGenerator::get_random_line(const vector<string>& lines) {
+string_view AbsurdGenerator::get_random_line(const vector<string_view>& lines) {
     return lines[randrange(0, static_cast<int>(lines.size()))];
 }
 
-string AbsurdGenerator::get_matched_line(const vector<string>& lines, const string& previous) {
+string_view AbsurdGenerator::get_matched_line(const vector<string_view>& lines, const string_view& previous) {
     auto it = upper_bound(lines.begin(), lines.end(), previous, reversed_last_word_comparator);
     if (it != lines.end()) {
         return *it;
--- absurdgenerator/absurd_generator.h
+++ absurdgenerator/absurd_generator.h
@@ -4,33 +4,35 @@
 #include <iterator>
 #include <vector>
 #include <string>
+#include <string_view>
 #include <map>
 #include <algorithm>
 #include <random>
 #include <chrono>
 
 using std::vector;
-using std::string;
+using std::string_view;
 
 class AbsurdGenerator {
 public:
 
-    AbsurdGenerator(const string& filename);
-    vector<string> generate_strophe();
+    AbsurdGenerator(const std::string& filename);
+    vector<string_view> generate_strophe();
 
 private:
 
-    void read(const string& filename);
-    void add_strophe(const vector<string>& lines);
+    void read(const std::string& filename);
+    void add_strophe(const vector<string_view>& lines);
     int randrange(int left, int right);
-    string get_random_line(const vector<string>& lines);
-    string get_matched_line(const vector<string>& lines, const string& previous);
-    static bool reversed_last_word_comparator(const string& first, const string& second);
+    string_view get_random_line(const vector<string_view>& lines);
+    string_view get_matched_line(const vector<string_view>& lines, const string_view& previous);
+    static bool reversed_last_word_comparator(const string_view& first, const string_view& second);
     
 private:
 
     std::mt19937 random_generator;
-    vector<vector<string>> lines_by_strophe;
+    vector<vector<string_view>> lines_by_strophe;
+    std::string buffer;
     
     static const unsigned STROPHE_SIZE;
     static const unsigned STROPHE_DEPENDENCY[];
